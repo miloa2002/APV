bc4cb9cadaa7c7ca89aacb9e4cf017505dcd214b
miloa2002 <agudelo.tapias@gmail.com>
2023-06-11 15:55:14 -0500

    hash password

--- backend/models/Veterinario.js
+++ backend/models/Veterinario.js
@@ -1,4 +1,5 @@
 import mongoose from "mongoose";
+import bcrypt from "bcrypt";
 
 const userSchema = new mongoose.Schema(
   {
@@ -35,6 +36,16 @@ const userSchema = new mongoose.Schema(
     timestamps: true,
   }
 );
+
+userSchema.pre('save', async function(next){
+
+  if(!this.isModified('password ')){
+    next();
+  }
+
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
+})
 
 const User = mongoose.model("User", userSchema);
 export default User;
